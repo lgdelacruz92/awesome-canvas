8f20de1a8dad4473142c7631469f9424306cee9e
lgdelacruz <bboyjacks@gmail.com>
2019-11-26 13:06:45 -0800

    Clean state

--- src/awesomecanvas/awesomecanvas.js
+++ src/awesomecanvas/awesomecanvas.js
@@ -1,8 +1,18 @@
 import React from "react";
-import * as MaterialUI from "@material-ui/core";
+import DNSContainer, { DNSImage } from "dns-container";
 
 const AwesomeCanvas = props => {
-  return <div></div>;
+  const { width, height } = props;
+  const children = React.Children.toArray(props.children);
+  children.forEach(c => {
+    const child = React.cloneElement(c);
+    console.log("The child", child);
+  });
+  return (
+    <DNSContainer width={width} height={height}>
+      {props.children}
+    </DNSContainer>
+  );
 };
 
 export default AwesomeCanvas;
---
+++ src/awesomecanvas/awesomeimage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { DNSImage } from "dns-container";
+
+const AwesomeImage = props => {
+  return <DNSImage></DNSImage>;
+};
+
+export default AwesomeImage;
