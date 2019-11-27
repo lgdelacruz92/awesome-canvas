1771e964dd87b0ff9d8b41f5a491c44071ea7543
lgdelacruz <bboyjacks@gmail.com>
2019-11-27 08:43:49 -0800

    Able to add image

--- src/awesomecanvas/awesomecanvas.js
+++ src/awesomecanvas/awesomecanvas.js
@@ -2,7 +2,6 @@ import React from "react";
 import DNSContainer from "dns-container";
 
 const AwesomeCanvas = props => {
-  console.log("children", props.children);
   return <DNSContainer {...props}>{props.children}</DNSContainer>;
 };
 
