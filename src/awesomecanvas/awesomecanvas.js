3751d78fbfd81bc80cade70541845a5e66cede93
lgdelacruz <bboyjacks@gmail.com>
2019-11-27 15:32:44 -0800

    Refactor clean up

--- src/awesomecanvas/awesomecanvas.js
+++ src/awesomecanvas/awesomecanvas.js
@@ -1,12 +1,9 @@
 import React from "react";
 import DNSContainer, { DNSImage } from "dns-container";
-import uuid from "react-uuid";
-
+import { copyState, initializeState, onUndo as undo } from "./utils";
 const AwesomeCanvas = props => {
   const _onChangeEnd = newData => {
     console.log("changed seem", newData);
-    state[newData.id].data = { ...newData };
-    setState({ ...state });
   };
 
   const _onRemove = dataId => {
@@ -21,100 +18,28 @@ const AwesomeCanvas = props => {
     console.log("seem move down", dataId);
   };
 
-  const [state, setState] = React.useState(() => {
-    const children = React.Children.toArray(props.children);
-
-    let startState = {};
-    children.forEach(child => {
-      let childId = child.props.data.id;
-      startState[childId] = {
-        data: child.props.data,
-        onChangeEnd: newData => {
-          _onChangeEnd(newData);
-          child.props.onChangeEnd(newData);
-        },
-        onRemove: () => {
-          _onRemove(childId);
-          child.props.onRemove();
-        },
-        onMoveUp: () => {
-          _onMoveUp(childId);
-          child.props.onMoveUp();
-        },
-        onMoveDown: () => {
-          _onMoveDown(childId);
-          child.props.onMoveDown();
-        }
-      };
-    });
-    return startState;
+  const initialState = initializeState({
+    props,
+    _onChangeEnd,
+    _onRemove,
+    _onMoveDown,
+    _onMoveUp
   });
-  const [history, setHistory] = React.useState([]);
-  const [historyPos, setHistoryPos] = React.useState(-1);
+  const [state, setState] = React.useState(initialState);
+  const [history, setHistory] = React.useState([initialState]);
 
   React.useEffect(() => {
-    let time = 0;
     const onUndo = e => {
-      const isCMDKey = e.key === "Meta";
-      if (isCMDKey) {
-        time = Date.now();
-      }
-
-      if (e.key === "z") {
-        const isPressedAtTheSameTime = Date.now() - time < 200;
-        if (isPressedAtTheSameTime) {
-          console.log("undo", historyPos);
-
-          if (historyPos > 0) {
-            let prevState = history[historyPos - 1];
-            let newState = {};
-            Object.keys(prevState).forEach(prevStateId => {
-              let prevStateData = prevState[prevStateId];
-              newState[prevStateId] = {
-                data: JSON.parse(JSON.stringify(prevStateData.data)),
-                onChangeEnd: prevStateData.onChangeEnd,
-                onRemove: prevStateData.onRemove,
-                onMoveUp: prevStateData.onMoveUp,
-                onMoveDown: prevStateData.onMoveDown
-              };
-            });
-            console.log("New State");
-            setState(newState);
-          }
-        }
-      }
+      undo(e);
     };
     document.addEventListener("keydown", onUndo);
     return () => document.removeEventListener("keydown", onUndo);
-  }, [historyPos]);
-
-  const copyState = () => {
-    let stateCopy = {};
-    Object.keys(state).forEach(stateId => {
-      let stateData = state[stateId];
-      stateCopy[stateId] = {
-        data: JSON.parse(JSON.stringify(stateData.data)),
-        onChangeEnd: stateData.onChangeEnd,
-        onRemove: stateData.onRemove,
-        onMoveUp: stateData.onMoveUp,
-        onMoveDown: stateData.onMoveDown
-      };
-    });
-    return stateCopy;
-  };
+  }, []);
 
   React.useEffect(() => {
-    setHistory(his => {
-      if (his.length > 4) {
-        his.splice(0, 1);
-      }
-
-      return [...his, copyState()];
-    });
-  }, [state]);
-
-  React.useEffect(() => {
-    setHistoryPos(history.length - 1);
+    let topHistory = history[history.length - 1];
+    let newState = copyState(topHistory);
+    setState(newState);
   }, [history]);
 
   return (
---
+++ src/awesomecanvas/utils.js
@@ -0,0 +1,61 @@
+import React from "react";
+export const copyState = item => {
+  let newCopy = {};
+  Object.keys(item).forEach(stateId => {
+    let itemData = item[stateId];
+    newCopy[stateId] = {
+      data: JSON.parse(JSON.stringify(itemData.data)),
+      onChangeEnd: itemData.onChangeEnd,
+      onRemove: itemData.onRemove,
+      onMoveUp: itemData.onMoveUp,
+      onMoveDown: itemData.onMoveDown
+    };
+  });
+  return newCopy;
+};
+
+export const initializeState = dep => {
+  const { props, _onChangeEnd, _onRemove, _onMoveDown, _onMoveUp } = dep;
+  const children = React.Children.toArray(props.children);
+
+  let startState = {};
+  children.forEach(child => {
+    let childId = child.props.data.id;
+    startState[childId] = {
+      data: child.props.data,
+      onChangeEnd: newData => {
+        _onChangeEnd(newData);
+        child.props.onChangeEnd(newData);
+      },
+      onRemove: () => {
+        _onRemove(childId);
+        child.props.onRemove();
+      },
+      onMoveUp: () => {
+        _onMoveUp(childId);
+        child.props.onMoveUp();
+      },
+      onMoveDown: () => {
+        _onMoveDown(childId);
+        child.props.onMoveDown();
+      }
+    };
+  });
+  return startState;
+};
+
+let time = 0;
+
+export const onUndo = e => {
+  const isCMDKey = e.key === "Meta";
+  if (isCMDKey) {
+    time = Date.now();
+  }
+
+  if (e.key === "z") {
+    const isPressedAtTheSameTime = Date.now() - time < 200;
+    if (isPressedAtTheSameTime) {
+      console.log("undo");
+    }
+  }
+};
